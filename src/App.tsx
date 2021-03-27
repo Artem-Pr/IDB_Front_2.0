e0d5bb5dbb3d2dec42e7b28ef422317d2ce50415
Artem Pryadkin <pryadkin1987@gmail.com>
2021-03-27 18:28:28 +0300

    fix format

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,14 @@
-import React from 'react'import { Layout } from 'antd'import Header from './app/Components/Header'function App() {  return (    <Layout>      <Header />    </Layout>  )}export default App
+import React from 'react'
+import { Layout } from 'antd'
+
+import Header from './app/Components/Header'
+
+function App() {
+  return (
+    <Layout>
+      <Header />
+    </Layout>
+  )
+}
+
+export default App
